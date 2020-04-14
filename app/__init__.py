150e16593aabea5c030040dcebaaab77c90d9588
dineahcohen <55610866+dineahcohen@users.noreply.github.com>
2020-04-14 00:58:25 -0500

    updated uri and procfile

--- app/__init__.py
+++ app/__init__.py
@@ -3,7 +3,7 @@ from flask_sqlalchemy import SQLAlchemy
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = "$up3rDup3R3K3Y"
-app.config['SQLALCHEMY_DATABASE_URI'] = "postgresql://project1:project1@localhost/project1"
+app.config['SQLALCHEMY_DATABASE_URI'] ='postgresql://rjzbqcvepeliow:f9ccc4d76bb5ba1d50bbb268cf2649da7784b01f334bba9f88590de6a1097058@ec2-34-197-212-240.compute-1.amazonaws.com:5432/d2c6fvcffkoc5u'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = True 
 app.config['UPLOAD_FOLDER'] = './app/static/uploads'
 
